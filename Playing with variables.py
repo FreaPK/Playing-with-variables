2ef77a05f5361dff5a4b5e13d4bd1079d8f982ed
Rahul Bhardwaj <98464340+FreaPK@users.noreply.github.com>
2026-01-01 14:50:53 +0530

    Add files via upload

---
+++ Playing with variables.py
@@ -0,0 +1,29 @@
+#playing with variables
+mrp=190
+print(mrp)
+
+#rules for creating a variable
+#Start with a lower case or underscore
+#Letters,numbers and underscores can be used but remember the latter point
+#They are case sensitive
+#Don't overwrite keywords
+#snake case cant - use space here so use underscore instead
+#They are reassignable
+
+user_iq=123
+age=user_iq/3
+print(user_iq)
+print(age)
+
+#A good convention write variables in capital to show that they are constants
+E=2.8
+PI=3.14
+
+#dunder method they shouldn't be muted
+__BCA='123'
+
+#simultaneously assigning values to variables
+a,b,c=1,2,3
+print(a)
+print(b)
+print(c)
